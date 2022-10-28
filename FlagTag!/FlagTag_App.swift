69822449a724d4111e46ba81fbc319ba40f0e929
heflinbh <heflinbh@miamioh.edu>
2022-10-28 17:34:52 -0400

    Initial Commit

---
+++ FlagTag!/FlagTag_App.swift
@@ -0,0 +1,17 @@
+//
+//  FlagTag_App.swift
+//  FlagTag!
+//
+//  Created by Benjamin Heflin on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagTag_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
